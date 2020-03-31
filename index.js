06849e4c7817a7db9ebc0330b25ce684e62c1134
Shaqqour <mshaqqourm@gmail.com>
2020-03-31 11:48:07 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let titleizeString = "";
+    string.split(" ").forEach(word => {
+      if(except.includes(word)) {
+        titleizeString += word + " ";
+      }
+      else {
+        titleizeString += this.capitalize(word) + " ";
+      }
+    });
+    return this.capitalize(titleizeString).slice(0, titleizeString.length-1);
+  }
+
 }
