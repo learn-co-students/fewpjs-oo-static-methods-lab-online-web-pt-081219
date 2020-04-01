8f4c332cdce176798854f98b229f3ba10fbbe4f7
Christian Cain <christian22cain@yahoo.com>
2020-04-01 15:08:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(input) {
+    return input.charAt(0).toUpperCase() + input.slice(1);
+  }
+
+  static sanitize(input) {
+    return input.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+  static titleize(input) {
+    let stringArray = input.split(" ")
+    let capitalArray = [];
+    let ommitChars = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    
+    stringArray.map(el => {
+
+      if (stringArray.indexOf(el) === 0) {
+        capitalArray.push(this.capitalize(el))
+        return
+      } else if (!ommitChars.includes(el)) {
+        capitalArray.push(this.capitalize(el))
+        return
+      } else {
+        capitalArray.push(el)
+      }
+    })
+
+    return capitalArray.join(" ")
+  }
 }
