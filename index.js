2284224de815b8b3b828a2911600d66a95163ee8
umberto palazzo <upalazzo93@gmail.com>
2020-04-02 21:09:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string){
+    let strings = string.split(' ')
+    let capStrings = strings.map(word =>{
+      if (["a", "the", "an", "but", "of", "and", "for", "at", "by", "from"].includes(word)){
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    })
+
+    capStrings[0] = this.capitalize(capStrings[0])
+
+    return capStrings.join(' ')
+  }
 }
